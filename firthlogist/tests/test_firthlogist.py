d930a84886d268ad1e1f85e050d1e9f30cf342ec
Jon Luo <20971593+jzluo@users.noreply.github.com>
2022-07-29 21:10:51 -0400

    add tests for load_sex2 and load_endometrial

--- firthlogist/tests/test_firthlogist.py
+++ firthlogist/tests/test_firthlogist.py
@@ -5,7 +5,7 @@ import pytest
 from numpy.testing import assert_allclose
 from sklearn.utils.estimator_checks import parametrize_with_checks
 
-from firthlogist import FirthLogisticRegression
+from firthlogist import FirthLogisticRegression, load_endometrial, load_sex2
 
 TEST_DIR = Path(__file__).parent
 
@@ -13,6 +13,23 @@ TEST_DIR = Path(__file__).parent
 @parametrize_with_checks([FirthLogisticRegression()])
 def test_estimator(estimator, check):
     return check(estimator)
+
+
+@pytest.mark.parametrize(
+    "loader_func, data_shape, target_shape, n_target, xname",
+    [
+        (load_sex2, (239, 6), (239,), (2,), ["age", "oc", "vic", "vicl", "vis", "dia"]),
+        (load_endometrial, (79, 3), (79,), (2,), ["NV", "PI", "EH"]),
+    ],
+)
+def test_loaders(loader_func, data_shape, target_shape, n_target, xname):
+    X, y, feature_names = loader_func()
+    assert isinstance(X, np.ndarray)
+    assert isinstance(y, np.ndarray)
+    assert X.shape == data_shape
+    assert y.shape == target_shape
+    assert np.unique(y).shape == n_target
+    assert feature_names == xname
 
 
 @pytest.fixture
